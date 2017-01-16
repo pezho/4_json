00c644cc38aec7104ab1310e0c97823b7a8a5e9e
pezho <pezho.i@mail.ru>
2017-01-16 16:30:02 +0200

    add first solution

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import os.path
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, encoding='utf-8') as f:
+        return json.load(f)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter path to your json file: ')
+    if not filepath.endswith('.json'):
+        print('Type of file most be json.')
+    else:
+        json_data = load_data(filepath)
+        pretty_print_json(json_data) if json_data else print('File empty')
+        
